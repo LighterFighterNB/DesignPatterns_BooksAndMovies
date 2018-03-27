925adac93899662037a6680dc32bc8666eec654a
Eric Smith <smith.eric.george@gmail.com>
2018-03-27 18:14:25 +0200

    Start on Catalog Item and some main tests

--- src/main/java/CatalogItem/CatalogItem.java
+++ src/main/java/CatalogItem/CatalogItem.java
@@ -1,4 +1,10 @@
 package CatalogItem;
 
 public interface CatalogItem {
+
+    String getTitle();
+    void setTitle(String title);
+
+    String getCreator();
+    void setCreator(String creator);
 }
